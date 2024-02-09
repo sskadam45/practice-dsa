16c910b29faff8716834ff87ef202993a093bb74
patilpri <pritam.patil@autodesk.com>
2024-02-09 10:44:53 +0530

    problems and code editor ui

--- src/components/workspace/playground/Playground.tsx
+++ src/components/workspace/playground/Playground.tsx
@@ -7,23 +7,79 @@ import { basicDark, basicLight, copilot, eclipse, githubDark, githubLight, vscod
 import { javascript } from '@codemirror/lang-javascript'
 import { java } from '@codemirror/lang-java'
 import { cpp } from '@codemirror/lang-cpp'
+import EditorFooter from './editor-footer/EditorFooter'
 type PlaygroundProps = {}
 
 const Playground = (props: PlaygroundProps) => {
+
+  const sampleCode = `function twoSum( nums, target) {
+  // write your code here
+
+};`;
+
   return (
     <>
-    <div className='flex flex-col bg-dark-layer-1 relative'>
+    <div className='flex flex-col bg-dark-layer-1 relative overflow-x-hidden'>
         <PreferenceNav/>
         <Split className='h-[calc(100vh-94px)]'    direction='vertical' sizes={[60,40]} minSize={60}>
             <div className='w-full overflow-auto'>
-                <CodeMirror value='const a=1'
+                <CodeMirror value={sampleCode}
                     theme={[vscodeDark,eclipse,basicDark,basicLight,githubDark,githubLight,copilot]}
                     extensions={[javascript(),java(),cpp()]}
                     style={{fontSize:16}}/>
             </div>
-            <div> Test cases </div>
-        <Playground/>
-  </Split>
+            <div className='w-full px-5 overflow-auto'>
+
+              <div className='flex h-10 items-center space-x-6'>
+                {/* heading */}
+                <div className='relative flex h-full flex-col justify-center cursor-pointer'>
+                  <div className='text-sm font-medium leading-5 text-white'> Test Cases</div>
+                  <hr className='absolute bottom-0 h-0.5 w-20 rounded-full border-none bg-white'/>
+                </div>
+
+                <div className='flex'>
+                  {/* case 1 */}
+                  <div className='mr-2 items-start mt-2 text-white'>
+                    <div className='flex flex-wrap items-center gap-y-4'>
+                      <div className='font-medium items-center transition-all focus: outline-none inline-flex bg-dark-fill-3
+                      hover:bg-dark-fill-2 rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>Case 1
+                      </div>
+                    </div>
+                  </div>
+
+                {/* c2 */}
+                  <div className='mr-2 items-start mt-2 text-white'>
+                    <div className='flex flex-wrap items-center gap-y-4'>
+                      <div className='font-medium items-center transition-all focus: outline-none inline-flex bg-dark-fill-3
+                      hover:bg-dark-fill-2 rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>Case 2
+                      </div>
+                    </div>
+                  </div>
+                  {/* c3 */}
+                  <div className='mr-2 items-start mt-2 text-white'>
+                    <div className='flex flex-wrap items-center gap-y-4'>
+                      <div className='font-medium items-center transition-all focus: outline-none inline-flex bg-dark-fill-3
+                      hover:bg-dark-fill-2 rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>Case 3
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* inp/op */}
+              <div className='font-semibold my-4'>
+                  <p className='text-sm font-medium mt-4 text-white'>Input:</p>
+                  <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                    nums:[2, 3, 5, 77, 8], target: 9
+                  </div>
+                  <p className='text-sm font-medium mt-4 text-white'>Output:</p>
+                  <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                    [0, 1]
+                  </div>
+                </div>
+            </div>
+        </Split>
+        {/* footer */}
+        <EditorFooter/>
     </div>
        
     </>
---
+++ src/utils/problems/index.ts
@@ -0,0 +1,19 @@
+import { Problem } from "../types/problemDTO";
+import { jumpGame } from "./jump-game";
+import { reverseLinkedList } from "./reverse-linked-list";
+import { search2DMatrix } from "./search-a-sd-matrix";
+import { twoSum } from "./two-sum";
+import { validParentheses } from "./valid-parentheses";
+
+
+interface ProblemMap {
+	[key: string]: Problem;
+}
+
+export const problems: ProblemMap = {
+	"two-sum": twoSum,
+	"reverse-linked-list": reverseLinkedList,
+	"jump-game": jumpGame,
+	"search-a-2d-matrix": search2DMatrix,
+	"valid-parentheses": validParentheses,
+};
